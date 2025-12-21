2f88c204bcc906eb784ad26595db68a0f6377a81
Mahmoud <forkan.saadaoui@gmail.com>
2025-12-21 21:59:22 +0100

    feat: Add initial user schema.

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -5,8 +5,38 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-    @Prop()
+    @Prop({ type: String, required: true, min: [3, 'Name must be at least 3 characters long'], max: [30, 'Name must be at most 30 characters long'] })
     name: string;
+
+    @Prop({ type: String, required: true, unique: true })
+    email: string;
+
+    @Prop({ type: String, required: true, min: [6, 'Password must be at least 6 characters long'], max: [20, 'Password must be at most 20 characters long'] })
+    password: string;
+
+    @Prop({ type: String, required: true, enum: ['user', 'admin'] })
+    role: string;
+
+    @Prop({ type: String })
+    avatar: string;
+
+    @Prop({ type: Number })
+    age: number;
+
+    @Prop({ type: Number, unique: true })
+    phone: number;
+
+    @Prop({ type: String })
+    address: string;
+
+    @Prop({ type: Boolean, enum: [true, false] })
+    active: string;
+
+    @Prop({ type: String })
+    verificationCode: string;
+
+    @Prop({ type: String, enum: ['male', 'female'] })
+    gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
